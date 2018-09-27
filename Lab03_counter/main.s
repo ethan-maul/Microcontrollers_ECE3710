2e15a1f12e64b1893ebd1cce348b38b6cda19537
ethan-maul <43353972+ethan-maul@users.noreply.github.com>
2018-09-27 11:41:05 -0600

    Add files via upload
    v2

--- Lab03_counter/main.s
+++ Lab03_counter/main.s
@@ -96,3 +96,152 @@
 
 
 ;*******************BEGIN INITIALIZATION**************************
+	INCLUDE core_cm4_constants.s		; Load Constant Definitions
+	INCLUDE stm32l476xx_constants.s     ; Load register values 
+
+	AREA    main, CODE, READONLY		;define code below, once on boar it cannot be modified
+	EXPORT	__main						; make __main visible to linker
+	ENTRY			
+				
+__main	PROC							;start main
+	
+	LDR r0, =RCC_BASE					
+	LDR r1, [r0, #RCC_AHB2ENR]			
+	ORR r1, r1, #RCC_AHB2ENR_GPIOEEN	
+	STR r1, [r0, #RCC_AHB2ENR]			
+	LDR r0, =GPIOE_BASE		
+	LDR r6, =0x05550000	
+	LDR r1, [r0, #GPIO_MODER]
+	AND r1, r6
+	STR r1, [r0, #GPIO_MODER]
+	LDR r6, =0xAAAAAAAA					;figure out what this 'masking' does
+	LDR r1, [r0, #GPIO_PUPDR] 			;adds a pull up, pull down reg to the input
+	ORR r1, r6 							;figure out what this does
+	STR r1, [r0, #GPIO_PUPDR]
+	LDR r6, =0x00000000					;clears input register
+	STR r6, [r0, #GPIO_ODR]			
+
+	LDR r2, =RCC_BASE		
+	LDR r3, [r2, #RCC_AHB2ENR]				
+	ORR r3, r3, #RCC_AHB2ENR_GPIOBEN		
+	STR r3, [r2, #RCC_AHB2ENR]			
+	LDR r2, =GPIOB_BASE					
+	LDR r3, [r2, #GPIO_MODER]
+	LDR r6, =0x00005050
+	AND r3, r6
+	STR r3, [r2, #GPIO_MODER]
+	LDR r6, =0xAAAAAAAA					;figure out what this 'masking' does
+	LDR r3, [r2, #GPIO_PUPDR] 			;adds a pull up, pull down reg to the input
+	ORR r3, r6 							;figure out what this does
+	STR r3, [r2, #GPIO_PUPDR]
+	LDR r6, =0x00000000	 				;clears input register
+	STR r6, [r2, #GPIO_ODR]
+				
+	;Joystick setup pins PA0 to PA3 & PA5
+	;first value sets input mode on MODER, then feeds input values through PUPDR to input
+	LDR r6, =0xFFFFF300 				;value for input mode on joystick
+	LDR r4, =RCC_BASE	
+	LDR r5, [r4, #RCC_AHB2ENR]				
+	ORR r5, r5, #RCC_AHB2ENR_GPIOAEN		
+	STR r5, [r4, #RCC_AHB2ENR]			
+	LDR r4, =GPIOA_BASE					
+	LDR r5, [r4, #GPIO_MODER]	
+	AND r5, r6 							;changes PA0-PA3 & PA5 to input mode
+	STR r5, [r4, #GPIO_MODER]
+	LDR r6, =0x8AA 						;figure out what this 'masking' does
+	LDR r5, [r4, #GPIO_PUPDR] 			;adds a pull up, pull down reg to the input
+	ORR r5, r6 							;figure out what this does
+	STR r5, [r4, #GPIO_PUPDR]
+	LDR r6, =0x00000000 				;clears input register
+	STR r6, [r4, #GPIO_IDR]
+	
+	LDR r0, =RCC_BASE					;turn HSI clk on
+	LDR r1, [r0, #RCC_CR]
+	ORR r1, #0x00000100
+	STR r1, [r0, #RCC_CR]
+	
+HSIclk
+	LDR r1, [r0, #RCC_CR]
+	LDR r6, =0x00000563	
+	CMP r1, r6							;check if HSI is ready
+	BEQ HSIclk
+	
+	LDR r0, =GPIOE_BASE	
+	
+loop
+	LDR r6, =0x30D40 	
+	mov r12, r6		 					;clk divider
+DELAY 
+	; center, stop
+	LDR r5, [r4, #GPIO_IDR]
+	LDR r6, =0x0000A001 				;value to compare joystick to check if center is pushed
+	CMP r5, r6 							;compare joystick to check value
+	BEQ stop 							;if equal, branch to appropriate logic
+	
+	; left, reset
+	LDR r6, =0x0000A002
+	CMP r5, r6
+	BEQ reset
+	
+	; right, start
+	LDR r6, =0x0000A004
+	CMP r5, r6
+	BEQ start
+	
+	SUBS r12, #1
+	CMP r12, #0x0
+	bne DELAY
+	
+	cmp r8, #1 ; If clock is disabled, restart the loop
+	BNE loop
+	ADD r9, #0x1
+	LDR r6, =0x400
+	CMP r9, r6
+	BNE display
+	LDR r9, =0x0
+	
+display
+	;load r9 into r10, mask the appropriate bits and set them to the right point of the output GPIOs
+	;bits 0 and 1 of counter, shift them and add them to r11 - to load into GPIO ports 2 and 3
+	MOV r10, r9
+	MOV r11, #0x0
+	AND r10, #0x3
+	LSL r10, #4
+	ADD r11, r10
+	;bits 2 and 3 of counter, shift them and add them to r11 - to load into the GPIO ports 6 and 7
+	MOV r10, r9
+	ORR r10, #0xC
+	LSL r10, #2
+	ADD r11, r10
+	STR r11, [r2, #GPIO_ODR]
+	
+	MOV r10, r9
+	AND r10, #0x3F0
+	LSL r10, #6
+	STR r10, [r0, #GPIO_ODR]
+	
+	B loop
+	
+start ; enable the clock
+	LDR r8, =0x1
+	b trigger
+
+stop ; disable the clock
+	LDR r8, =0x0
+	b trigger
+
+reset ; set the counter to 0
+	LDR r9, =0x0
+	b display
+	
+trigger ;debouncer
+	LDR r5, [r4, #GPIO_IDR]
+	CMP r5, #0x0000A000 ;joystick press must be zero for program to leave loop
+	BEQ loop
+	B trigger
+	
+	ENDP
+					
+	ALIGN
+
+	END
